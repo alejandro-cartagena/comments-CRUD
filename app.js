48556ff99b7f9079b56022de96da4047fb95ced3
Alejandro <alejandrocartagena68@gmail.com>
2024-02-11 16:51:16 -0500

    Created CRUD routes for comments

--- app.js
+++ app.js
@@ -24,13 +24,13 @@ let comments = [
     },
     {
         id: uuid(),
-        username: 'tren_brah',
-        comment: 'Injecting steroids is like putting rocket fuel in your muscles. Just remember, even astronauts have to come back down to Earth eventually! 🚀😅💪'
+        username: 'sarmsgod69',
+        comment: 'Injecting steroids is like putting rocket fuel in your muscles. You feel like a goddamn astronaut baby! 🚀💪'
     },
     {
         id: uuid(),
-        username: 'sarms_goblin69',
-        comment: 'Bro, FUCK BEING NATTY'
+        username: 'tren_twins',
+        comment: 'Bro, STOP BEING NATTY'
     }
 
 ]
@@ -42,6 +42,42 @@ app.get('/', (req, res) => {
 app.get('/comments', (req, res) => {
     res.render('comments', { comments })
 })
+
+app.get('/comments/new', (req, res) => {
+    res.render('comments/new')
+})
+
+app.post('/comments', (req, res) => {
+    const { username, comment } = req.body
+    comments.push({ id: uuid(), username, comment })
+    res.redirect('/comments')
+})
+
+app.get('/comments/:id', (req, res) => {
+    const { id } = req.params
+    const comment = comments.find(c => c.id === id)
+    res.render('comments/show', { comment })
+})
+
+app.get('/comments/:id/edit', (req, res) => {
+    const { id } =  req.params
+    const comment = comments.find(c => c.id === id)
+    res.render('comments/edit', { comment })
+})
+
+app.patch('/comments/:id', (req, res) => {
+    const { id } = req.params
+    const comment = comments.find(c => c.id === id)
+    comment.comment = req.body.comment
+    res.redirect('/comments')
+})
+
+app.delete('/comments/:id', (req, res) => {
+    const { id } = req.params
+    comments = comments.filter(c => c.id !== id) 
+    res.redirect('/comments')
+})
+
 
 app.listen(3000, () => {
     console.log("Server running on port 3000...")
